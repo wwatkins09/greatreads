e7afb62da5550bc9361dd368131330831500a651
Will Watkins <willwatkins9@gmail.com>
2018-01-02 11:39:52 -0500

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+  validates :username, :img_url, :password_digest, :session_token, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+  after_initialize :ensure_session_token
+
+  has_many :bookshelves
+  has_many :reviews
+  has_many :books,
+  through: :bookshelves
+
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(this.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password(password)
+      user
+    else
+      nil
+    end
+  end
+
+
+end
