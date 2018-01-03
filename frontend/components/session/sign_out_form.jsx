027db710f3209ad70c455f833f2cf78f6009a869
Will Watkins <willwatkins9@gmail.com>
2018-01-03 11:03:40 -0500

    Add username to navbar when signed in

--- frontend/components/session/sign_out_form.jsx
+++ frontend/components/session/sign_out_form.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 class SignOutForm extends React.Component {
 
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="navbar-form">
+        <p>Hello, {this.props.currentUser.username}</p>
         <button onClick={this.props.signOut}>Sign Out</button>
       </div>
     );
