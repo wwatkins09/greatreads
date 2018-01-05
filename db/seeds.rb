d4a375e78fc1c01ce614beeaaa076dc3ec381239
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-05 09:42:24 -0500

    Add more users and bookshelves to seed, install Faker to generate books later on

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,11 @@ Bookshelf.create(name: "Southern Gothic", user_id: User.find_by_credentials("Wil
 User.create(username: "Will2", password: "starwars")
 Bookshelf.create(name: "Fantasy", user_id: User.find_by_credentials("Will2", "starwars").id)
 Bookshelf.create(name: "Short story collections", user_id: User.find_by_credentials("Will2", "starwars").id)
+
+User.create(username: "Will3", password: "starwars")
+Bookshelf.create(name: "Nonfiction", user_id: User.find_by_credentials("Will3", "starwars").id)
+Bookshelf.create(name: "Comedy", user_id: User.find_by_credentials("Will3", "starwars").id)
+
+User.create(username: "Will4", password: "starwars")
+Bookshelf.create(name: "History", user_id: User.find_by_credentials("Will4", "starwars").id)
+Bookshelf.create(name: "Sci-fi", user_id: User.find_by_credentials("Will4", "starwars").id)
