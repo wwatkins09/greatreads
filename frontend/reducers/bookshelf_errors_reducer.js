b9860804428e1513cb716e4710e7639a2583b740
Will Watkins <willwatkins9@gmail.com>
2018-01-05 10:30:45 -0500

    Clear bookshelf errors after creation of new bookshelf

--- frontend/reducers/bookshelf_errors_reducer.js
+++ frontend/reducers/bookshelf_errors_reducer.js
@@ -1,10 +1,12 @@
-import {RECEIVE_BOOKSHELF_ERRORS, CLEAR_BOOKSHELF_ERRORS} from '../actions/bookshelf_actions';
+import {RECEIVE_BOOKSHELF_ERRORS, CLEAR_BOOKSHELF_ERRORS, RECEIVE_BOOKSHELF} from '../actions/bookshelf_actions';
 
 const bookshelfErrorsReducer = function(oldState=[], action) {
   switch(action.type) {
     case RECEIVE_BOOKSHELF_ERRORS:
       return action.errors;
     case CLEAR_BOOKSHELF_ERRORS:
+      return [];
+    case RECEIVE_BOOKSHELF:
       return [];
     default:
       return oldState;
