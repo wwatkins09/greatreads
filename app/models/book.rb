80ae4aec8834db8f4c5aa6b8eb6938510ed596d2
yo <yo>
2018-01-06 16:38:29 -0500

    Fix typo in BookshelfOwnership associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   validates :title, :author, :year, :average_score, presence: true
-  has_many :bookshelf_memberships
-  has_many :bookshelves, through: :bookshelf_memberships
+  has_many :bookshelf_ownerships
+  has_many :bookshelves, through: :bookshelf_ownerships
 
   has_attached_file :cover, default_url: "missing.jpeg"
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
