400544d2f3df3bd5392251e33a77a0ec5c274935
Will Watkins <willwatkins9@gmail.com>
2018-01-07 17:42:14 -0500

    Add more book covers, book index styling

--- app/views/api/books/index.json.jbuilder
+++ app/views/api/books/index.json.jbuilder
@@ -5,5 +5,7 @@
       json.author book.author
       json.year book.year
       json.averageScore book.average_score
+      json.bookshelfIds book.bookshelf_ids
+      json.coverUrl asset_path(book.cover.url)
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,8 +27,17 @@ User.create(username: "Will4", password: "starwars")
 Bookshelf.create(name: "History", user_id: User.find_by_credentials("Will4", "starwars").id)
 Bookshelf.create(name: "Sci-fi", user_id: User.find_by_credentials("Will4", "starwars").id)
 
-Book.create(title: "Anna Karenina", author: "Tolstoy", year: 1873, average_score: 5)
-Book.create(title: "Crime and Punishment", author: "Dostoevsky", year: 1866, average_score: 4.5)
+book1 = Book.new(title: "Anna Karenina", author: "Leo Tolstoy", year: 1873, average_score: 5)
+book2 = Book.new(title: "Crime and Punishment", author: "Fyodor Dostoevsky", year: 1866, average_score: 4.5)
+book3 = Book.new(title: "As I Lay Dying", author: "William Faulkner", year: 1930, average_score: 5)
+
+book1.cover = File.open("app/assets/images/anna_karenina.jpg")
+book2.cover = File.open("app/assets/images/crime_and_punishment.jpg")
+book3.cover = File.open("app/assets/images/as_i_lay_dying.jpg")
+
+book1.save!
+book2.save!
+book3.save!
 
 BookshelfOwnership.create(
   bookshelf_id: Bookshelf.find_by(name: "Russian Novels", user_id: User.find_by_credentials("Will", "starwars").id).id,
@@ -38,4 +47,9 @@ BookshelfOwnership.create(
 BookshelfOwnership.create(
   bookshelf_id: Bookshelf.find_by(name: "Russian Novels", user_id: User.find_by_credentials("Will", "starwars").id).id,
   book_id: Book.find_by(title: "Crime and Punishment").id
+)
+
+BookshelfOwnership.create(
+  bookshelf_id: Bookshelf.find_by(name: "Southern Gothic", user_id: User.find_by_credentials("Will", "starwars").id).id,
+  book_id: Book.find_by(title: "As I Lay Dying").id
 )
