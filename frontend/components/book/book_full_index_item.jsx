0be5f34057fa4ed9fbeb65a20af03cf4fe39d8b2
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-08 17:22:14 -0500

    Remove delete button from book full index

--- frontend/components/book/book_full_index_item.jsx
+++ frontend/components/book/book_full_index_item.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-class BookIndexItem extends React.Component {
+class BookFullIndexItem extends React.Component {
 
   constructor(props) {
     super(props);
-    this.handleRemove = this.handleRemove.bind(this);
   }
 
-  handleRemove(event) {
-    event.preventDefault();
-    debugger
-  }
 
   render() {
     return (
@@ -26,12 +21,9 @@ class BookIndexItem extends React.Component {
         </td>
         <td className="table-field-author">{this.props.book.author}</td>
         <td className="table-field-avg-score">{this.props.book.averageScore}</td>
-        <td className="table-field-delete">
-          <button onClick={this.handleRemove}>Remove book</button>
-        </td>
       </tr>
     );
   }
 }
 
-export default BookIndexItem;
+export default BookFullIndexItem;
