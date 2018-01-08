e93eb32d047761f88e77bb612fac860ca577ad09
Will Watkins <willwatkins9@gmail.com>
2018-01-08 11:55:24 -0500

    Add button background image

--- frontend/components/book/book_show_container.js
+++ frontend/components/book/book_show_container.js
@@ -1,24 +1,30 @@
 import {connect} from 'react-redux';
 import BookShow from './book_show';
 import {fetchBook} from '../../actions/book_actions';
+import {fetchUserBookshelves} from '../../actions/bookshelf_actions';
 import {withRouter} from 'react-router-dom';
 
 const mapStateToProps = function (state, ownProps) {
   let book = state.entities.books[ownProps.match.params.bookId];
     if (book) {
-
     } else {
       book = {title: "", author: "", year: null, average_score: null, coverUrl: ""};
     }
+    if (state.entities.users[state.session.currentUserId]) {
+      
+    }
+
   return {
     bookId: ownProps.match.params.bookId,
-    book
+    book,
+    currentUserId: state.session.currentUserId
   };
 };
 
 const mapDispatchToProps = function (dispatch) {
   return {
-    fetchBook: (bookId) => dispatch(fetchBook(bookId))
+    fetchBook: (bookId) => dispatch(fetchBook(bookId)),
+    fetchUserBookshelves: (userId) => dispatch(fetchUserBookshelves(userId))
   };
 };
 
