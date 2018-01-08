afc44a26d29b0d3f421b7d83da05eb54484650bd
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-08 13:15:33 -0500

    Add bookshelf ownership functionality on frontend - reducer, actions, etc.

--- frontend/actions/bookshelf_ownership_actions.js
+++ frontend/actions/bookshelf_ownership_actions.js
@@ -1,6 +1,7 @@
 import * as APIBookshelfOwnershipUtil from '../util/bookshelf_ownership_util.js';
 
 export const RECEIVE_BOOKSHELF_OWNERSHIP = 'RECEIVE_BOOKSHELF_OWNERSHIP';
+export const RECEIVE_BOOKSHELF_OWNERSHIPS = 'RECEIVE_BOOKSHELF_OWNERSHIPS';
 
 export const createBookshelfOwnership = function(bookshelfOwnership) {
   return function(dispatch) {
@@ -10,9 +11,24 @@ export const createBookshelfOwnership = function(bookshelfOwnership) {
   };
 };
 
+export const fetchBookshelfOwnershipsByBookshelfId = function(bookshelfId) {
+  return function(dispatch) {
+    return APIBookshelfOwnershipUtil.fetchBookshelfOwnershipsByBookshelfId(bookshelfId).then((bookshelfOwnerships) => {
+      dispatch(receiveBookshelfOwnerships(bookshelfOwnerships));
+    });
+  };
+};
+
 export const receiveBookshelfOwnership = function(bookshelfOwnership) {
   return {
     type: RECEIVE_BOOKSHELF_OWNERSHIP,
     bookshelfOwnership
+  };
+};
+
+export const receiveBookshelfOwnerships = function(bookshelfOwnerships) {
+  return {
+    type: RECEIVE_BOOKSHELF_OWNERSHIPS,
+    bookshelfOwnerships
   };
 };
--- frontend/greatreads.jsx
+++ frontend/greatreads.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-import merge from 'lodash/merge';
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -15,7 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore();
   }
   window.store = store;
-  window.merge = merge;
 
   const rootEl = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, rootEl);
