b0ca85f7fbc190612c21dfbc4a52261ef6983cb8
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-09 16:44:17 -0500

    Add review modal form functionality

--- frontend/components/modals/review/review_modal.jsx
+++ frontend/components/modals/review/review_modal.jsx
@@ -4,8 +4,14 @@ class ReviewModal extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {userId: this.props.currentUserId, bookId: this.props.book.id, score: null, body: ""};
 
+    this.handleBodyChange = this.handleBodyChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleBodyChange(event) {
+    this.setState({body: event.currentTarget.value});
   }
 
   handleSubmit(event) {
@@ -17,12 +23,33 @@ class ReviewModal extends React.Component {
 
     return (
       <div className="review-modal">
-        <p className="review-modal-title">{this.props.book.title}</p>
-        <p className="review-modal-author">by {this.props.book.author}</p>
-        <p className="review-modal-rating">My rating: </p>
-        <p className="review-modal-body">What did you think?</p>
-
-        <button onClick={this.handleSubmit}>Submit review!</button>
+        <main className="review-modal-content">
+          <form onSubmit={this.handleSubmit}>
+            <p className="review-modal-title">{this.props.book.title}</p>
+            <p className="review-modal-author">by {this.props.book.author}</p>
+            <p className="review-modal-rating">My rating: </p>
+            <label className="radio">
+            <input type="radio" name="score" value="1"></input>Didn't like it
+          </label>
+          <label className="radio">
+            <input type="radio" name="score" value="2"></input>It was OK
+          </label>
+          <label className="radio">
+            <input type="radio" name="score" value="3"></input>Liked it
+          </label>
+          <label className="radio">
+            <input type="radio" name="score" value="4"></input>Really liked it
+          </label>
+          <label className="radio">
+            <input type="radio" name="score" value="5"></input>It was amazing
+          </label>
+            <div className="review-modal-body">
+              <p>What did you think?</p>
+              <textarea onChange={this.handleBodyChange}>{this.state.body}</textarea>
+            </div>
+          <button>Submit review!</button>
+        </form>
+      </main>
       </div>
     );
   } else {
