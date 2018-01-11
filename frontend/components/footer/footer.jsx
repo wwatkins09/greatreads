cf37ef558df5e8939eb877a1965d88ce17930d79
Will Watkins <willwatkins9@gmail.com>
2018-01-11 13:45:11 -0500

    Add copyright info

---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default function () {
+
+
+  return (
+    <main>
+      <div>Icons made by <a href="https://www.flaticon.com/authors/egor-rumyantsev" title="Egor Rumyantsev">Egor Rumyantsev</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> are licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    </main>
+  );
+}
