6bd25128341daea356e8e0df92959e550fba6937
Will Watkins <willwatkins9@gmail.com>
2018-01-11 10:08:30 -0500

    Style homepage quote, fix ordering of bookshelves

--- frontend/components/bookshelf/bookshelves_list.jsx
+++ frontend/components/bookshelf/bookshelves_list.jsx
@@ -3,7 +3,11 @@ import BookshelfIndexItem from './bookshelf_index_item';
 
 export default function ({bookshelves}) {
 
-  const bookshelvesList = bookshelves.map((bookshelf) => {
+  const sortNumber = function (bookshelf1, bookshelf2) {
+    return bookshelf1.id - bookshelf2.id;
+  };
+
+  const bookshelvesList = bookshelves.sort(sortNumber).map((bookshelf) => {
     if (bookshelf) {
       return (<BookshelfIndexItem key={bookshelf.id} bookshelf={bookshelf} />);
     }
