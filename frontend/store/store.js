ea6c55b0fb7039065e1dc18e5c6c3d816dcb91cb
Will <willwatkins9@gmail.com>
2018-01-15 17:37:19 -0500

    Remove development-specific parts of code

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,11 +3,18 @@ import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+constmiddlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger)
+}
+
 const configureStore = (preloadedState = {}) => {
   return createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   );
 };
 
