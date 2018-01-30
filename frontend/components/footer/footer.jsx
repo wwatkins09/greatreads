5a1e09e2d931a9c408fa7e2c068db757bc2f0b03
Will <willwatkins9@gmail.com>
2018-01-30 15:47:45 -0500

    Add personal links to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -13,8 +13,15 @@ export default function ({currentUserId}) {
 
   return (
     <main className={footerClass}>
-      <p className="logo-credit">Logo designed by Flora Hollifield</p>
-      <div>Icons made by <a href="https://www.flaticon.com/authors/egor-rumyantsev" title="Egor Rumyantsev">Egor Rumyantsev</a>, <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> are licensed by <a href="https://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+      <span className="credits">
+        <p className="logo-credit">Logo designed by Flora Hollifield</p>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/egor-rumyantsev" title="Egor Rumyantsev">Egor Rumyantsev</a>, <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> are licensed by <a href="https://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+      </span>
+      <span className="personal-links">
+        <a href="http://will-watkins.com/">Profile</a>
+        <a href="https://github.com/wwatkins09">Github</a>
+        <a href="https://www.linkedin.com/in/will-watkins/">LinkedIn</a>
+      </span>
     </main>
   );
 }
