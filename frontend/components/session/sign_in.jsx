bb8506d3bcd9c534f4c7cef01a646752abd75b5f
Will <willwatkins9@gmail.com>
2018-02-16 15:18:53 -0500

    Refactor sign in, style navbar

--- frontend/components/session/sign_in.jsx
+++ frontend/components/session/sign_in.jsx
@@ -4,11 +4,53 @@ class SignIn extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {username: "", password: ""};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(field) {
+    return (event) => {
+      this.setState({[field]: event.target.value});
+    };
+  }
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.signIn(this.state).then(() => {}, () => {
+      this.setState( {username: "", password: ""} )
+      });
+  }
+
+  handleDemoSignin(event) {
+    event.preventDefault();
+
+    this.props.signIn({username: "Demo User", password: "starwars"})
+  }
+
+  componentWillUnmount() {
+    this.props.clearSessionAndUserErrors();
   }
 
   render() {
+    const sessionErrorsList = this.props.sessionErrors.map((error, idx) => {
+      return (<li className="session-error" key={idx}>{error}</li>);
+    });
+    
     return (
-      <div>working!!!</div>
+      <content className="signin">
+        <form className="navbar-form" onSubmit={this.handleSubmit}>
+            <input className="input-field" onChange={this.handleChange('username')} type="text" value={this.state.username} placeholder="Username"></input>
+            <input className="input-field" onChange={this.handleChange('password')} type="password" value={this.state.password} placeholder="Password"></input>
+            <button className="signin-button">Log In</button>
+        </form>
+        <ul className="session-errors-list">
+          {sessionErrorsList}
+        </ul>
+    </content>
     );
   }
 
--- frontend/components/session/sign_in_container.js
+++ frontend/components/session/sign_in_container.js
@@ -1,16 +1,18 @@
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import SignIn from './sign_in';
+import {signIn, clearSessionAndUserErrors} from '../../actions/session_actions';
 
 const mapStateToProps = (state) => {
   return {
-
+    sessionErrors: state.errors.session
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    signIn: (credentials) => dispatch(signIn(credentials)),
+    clearSessionAndUserErrors: () => dispatch(clearSessionAndUserErrors())
   };
 };
 
