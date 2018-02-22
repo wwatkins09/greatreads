58cf655e72855d88391997e8293ec5c74abb54b5
Will <willwatkins9@gmail.com>
2018-02-22 12:39:10 -0500

    begin adding demo login

--- frontend/components/session/sign_in.jsx
+++ frontend/components/session/sign_in.jsx
@@ -20,7 +20,9 @@ class SignIn extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
 
-    this.props.signIn(this.state).then(() => {}, () => {
+    this.props.signIn(this.state).then(() => {
+      this.props.history.push('/');
+    }, () => {
       this.setState( {username: "", password: ""} )
       });
   }
@@ -28,7 +30,9 @@ class SignIn extends React.Component {
   handleDemoSignin(event) {
     event.preventDefault();
 
-    this.props.signIn({username: "Demo User", password: "starwars"})
+    this.props.signIn({username: "Demo User", password: "starwars"}).then(() => {
+      this.props.push('/');
+    })
   }
 
   componentWillUnmount() {
@@ -44,11 +48,17 @@ class SignIn extends React.Component {
       <main className="signin">
         <content className="signin-content">
           <h2>Sign in</h2>
-          <form className="signin-form" onSubmit={this.handleSubmit}>
+          <div className="signin-form">
               <input className="input-field" onChange={this.handleChange('username')} type="text" value={this.state.username} placeholder="Username"></input>
               <input className="input-field" onChange={this.handleChange('password')} type="password" value={this.state.password} placeholder="Password"></input>
-              <button className="signin-button">Log In</button>
-          </form>
+              <span className="signin-button" onClick={this.handleSubmit}>Log In</span>
+              <span className="signin-or">
+                <div id="line-left" className="signin-line"></div>
+                <p>or</p>
+                <div id="line-right" className="signin-line"></div>
+
+              </span>
+          </div>
           <ul className="session-errors-list">
             {sessionErrorsList}
           </ul>
