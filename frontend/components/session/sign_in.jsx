72a71a1aeee228d04eb83e6ad5b384a65dfd9984
Will <willwatkins9@gmail.com>
2018-02-22 12:09:41 -0500

    Begin styling signin

--- frontend/components/session/sign_in.jsx
+++ frontend/components/session/sign_in.jsx
@@ -39,18 +39,21 @@ class SignIn extends React.Component {
     const sessionErrorsList = this.props.sessionErrors.map((error, idx) => {
       return (<li className="session-error" key={idx}>{error}</li>);
     });
-    
+
     return (
-      <content className="signin">
-        <form className="navbar-form" onSubmit={this.handleSubmit}>
-            <input className="input-field" onChange={this.handleChange('username')} type="text" value={this.state.username} placeholder="Username"></input>
-            <input className="input-field" onChange={this.handleChange('password')} type="password" value={this.state.password} placeholder="Password"></input>
-            <button className="signin-button">Log In</button>
-        </form>
-        <ul className="session-errors-list">
-          {sessionErrorsList}
-        </ul>
-    </content>
+      <main className="signin">
+        <content className="signin-content">
+          <h2>Sign in</h2>
+          <form className="signin-form" onSubmit={this.handleSubmit}>
+              <input className="input-field" onChange={this.handleChange('username')} type="text" value={this.state.username} placeholder="Username"></input>
+              <input className="input-field" onChange={this.handleChange('password')} type="password" value={this.state.password} placeholder="Password"></input>
+              <button className="signin-button">Log In</button>
+          </form>
+          <ul className="session-errors-list">
+            {sessionErrorsList}
+          </ul>
+        </content>
+    </main>
     );
   }
 
