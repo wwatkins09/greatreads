f8ea558609a1391a0c9068a47731f8ce6a251348
Will <willwatkins9@gmail.com>
2018-02-23 19:12:12 -0500

    Begin adding example books

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -22,18 +22,13 @@ class Homepage extends React.Component {
             <p>Demo</p>
           </span>
         </content>
-        <article className="homepage-ideas">
-          <span>
-            <p>Looking for something new?</p>
-            <p>Browse our collection of books and see if something catches your eye. You can make your own bookshelves to sort books by genre, time period, country - whatever you want!</p>
-          </span>
-          <span>
-            <p>Can't remember all the books you've read?</p>
-            <p>Keep track of what books you've already read, as well as books you're reading now or want to read in the future.</p>
-          </span>
-          <span>
-            <p>Have an opinion?</p>
-            <p>Leave a review to show whether you couldn't put it down or couldn't stand it. If you're not sure whether it's worth a try, check out what other users have said.</p>
+        <article className="homepage-discover">
+          <h4>What will <em>you</em> discover?</h4>
+          <span className="homepage-discover-example">
+            <content className="homepage-example-before">
+              <p>Because Flora liked...</p>
+              <span className="homepage-book-list">books here</span>
+            </content>
           </span>
         </article>
         <span className="homepage-registration">
