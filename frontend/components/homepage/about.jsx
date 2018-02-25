55bdd807a894a5ecc6d04ac0b4ee6df7e4ea6b55
Will <willwatkins9@gmail.com>
2018-02-24 19:10:00 -0500

    Add homepage content

--- frontend/components/homepage/about.jsx
+++ frontend/components/homepage/about.jsx
@@ -5,6 +5,8 @@ const About = (props) => {
     <div>
       <h2>About Greatreads</h2>
       <p>Greatreads is a web-app inspired by Goodreads that allows users to discover, organize, and review various books. It operates with Ruby on Rails and a Postgresql database on the backend, as well as with React and Redux architecture on the frontend.</p>
+      <p>The code for this site can be found</p><a href="https://github.com/wwatkins09/greatreads">here</a><p>.</p>
+      <p>To see more of my work, visit my profile site</p><a href="http://will-watkins.com">here</a><p>.</p>
     </div>
   );
 };
--- frontend/components/homepage/contact.jsx
+++ frontend/components/homepage/contact.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Contact = (props) => {
   return (
-    <div>contact</div>
+    <div>
+      <h1>Contact</h1>
+      <p>If you have any questions or comments about the site, please send them to the following email address: willwatkins9@gmail.com</p>
+    </div>
   );
 }
 
